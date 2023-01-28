6bd6578c32a452a725ab59f247cd24c241f580cc
Aniketjiotode <aniketjiotode07@gmail.com>
2023-01-28 13:25:16 +0530

    Added: uc1-checking emoplyee attendance

---
+++ EmpolyeeWage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Empolyee_Wage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee Wage App");
+            Console.WriteLine("Enter Employee Name: ");
+            string name = Console.ReadLine();
+            Random random = new Random();
+            int isPresent = random.Next(0, 2);
+
+            if (isPresent == 0)
+            {
+                Console.WriteLine(name + " is Absent");
+            }
+            else
+            {
+                Console.WriteLine(name + " is Present" );
+            }
+        }
+    }
+}
